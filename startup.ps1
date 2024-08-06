579ce3a05fa012ecb928c4b593ab80560a4faa2e
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2024-08-06 13:34:11 -0700

    Set up torch with CUDA

--- startup.ps1
+++ startup.ps1
@@ -1,5 +1,6 @@
 python -m venv .venv
 .\.venv\Scripts\activate.ps1
 python -m pip install --upgrade pip
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install -r .\requirements.txt
 python .\main.py
