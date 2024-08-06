66e07690b99b1429f359bb9dafad3b9eb1ef1bdd
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2024-08-07 01:26:54 +0300

    Minor fix

--- CUDAcheck.py
+++ CUDAcheck.py
@@ -3,6 +3,7 @@ import torch
 if torch.cuda.is_available():
     device = "cuda:0"
     print("CUDA is available. Using GPU.")
+    print(f"Torch CUDA version {torch.version.cuda}")
 else:
     device = "cpu"
     print("CUDA is not available. Using CPU.")
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 if torch.cuda.is_available():
     device = "cuda:0"
     print("CUDA is available. Using GPU.")
+    print(f"Torch CUDA version {torch.version.cuda}")
 else:
     device = "cpu"
     print("CUDA is not available. Using CPU.")
